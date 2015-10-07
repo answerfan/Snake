6bd1ccb53d25d847c983dce8a16fd37d254a870d
answerfan <Spawn333>
2015-10-07 17:44:57 +0600

    4

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,10 +10,10 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
+            Point p1 = new Point(1, 3, '!' );
             p1.Draw();
 
-            Point p2 = new Point(4, 5, '#');
+            Point p2 = new Point(4, 5, '*' );
             p2.Draw();
            
             Console.ReadLine();
