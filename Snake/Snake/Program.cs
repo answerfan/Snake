47188cc13ec6ed5216cd4c6a43e70b2a5b82062e
answerfan <Spawn333>
2015-10-08 16:33:14 +0600

    6+

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -15,7 +15,51 @@ namespace Snake
 
             Point p2 = new Point(4, 5, '*' );
             p2.Draw();
-           
+
+            Point p3 = new Point(2, 7, '?');
+            p3.Draw();
+
+            Point p4 = new Point(7, 12, '@');
+            p4.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+            List<char> charL = new List<char>();
+            charL.Add('*');
+            charL.Add('!');
+
+            char b = charL[1];
+            char c = charL[0];
+
+            foreach(char j in charL)
+            {
+                Console.WriteLine(j);
+            }
+
+            List<Point> p1l = new List<Point>();
+            p1l.Add(p2);
+            p1l.Add(p3);
+
             Console.ReadLine();
         }
         
